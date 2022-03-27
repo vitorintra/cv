df0937894896dbfc861b86349576cd86ada94a24
vintra <vitintra63@gmail.com>
2022-03-27 02:27:50 -0300

    feat: storing theme

--- script.js
+++ script.js
@@ -12,31 +12,43 @@ const typeWriter = () => {
 
 typeWriter();
 
-const moonIcon = '<i class="uil uil-moon"></i>';
-const sunIcon = '<i class="uil uil-sun"></i>';
+const moonIcon = "<i class='uil uil-moon'></i>";
+const sunIcon = "<i class='uil uil-sun'></i>";
 const themeButton = document.querySelector(".change-theme");
 const elementsWithBackcgroundToChange =
   document.querySelectorAll(".back-theme");
 const elementsWithTextToChange = document.querySelectorAll(".text-theme");
-themeButton.innerHTML = sunIcon;
+
+const storedTheme = localStorage.getItem("theme");
+
+if (!storedTheme || storedTheme === "dark") {
+  localStorage.setItem("theme", "dark");
+  themeButton.innerHTML = sunIcon;
+
+  elementsWithBackcgroundToChange.forEach((element) => {
+    element.classList.remove("back-theme");
+  });
+
+  elementsWithTextToChange.forEach((element) => {
+    element.classList.remove("text-theme");
+  });
+} else {
+  localStorage.setItem("theme", "light");
+  themeButton.innerHTML = moonIcon;
+}
 
 const setButtonIcon = () => {
   const currentIconClass = themeButton.children[0].classList[1];
+
   if (currentIconClass === "uil-sun") {
+    localStorage.setItem("theme", "light");
     themeButton.innerHTML = moonIcon;
     return;
   }
 
+  localStorage.setItem("theme", "dark");
   themeButton.innerHTML = sunIcon;
 };
-
-elementsWithBackcgroundToChange.forEach((element) => {
-  element.classList.remove("back-theme");
-});
-
-elementsWithTextToChange.forEach((element) => {
-  element.classList.remove("text-theme");
-});
 
 themeButton.addEventListener("click", () => {
   setButtonIcon();
